eeeb5f1052133431810cb298021cd1253f291871
Muhammad Linggar Ryanidha <Muhammad Linggar Ryanidha>
2023-03-06 01:04:54 +0700

    Minor update

--- jarkop_driver_app/lib/home/order.dart
+++ jarkop_driver_app/lib/home/order.dart
@@ -12,8 +12,53 @@ class OrderPage extends StatefulWidget {
 class _OrderPageState extends State<OrderPage> {
   @override
   Widget build(BuildContext context) {
+    final sw = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Text("Halo"),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(),
+                height: 90,
+                width: sw,
+                decoration: BoxDecoration(
+                  color: Color(0xffEACDA2)
+                ),
+                child: Row(
+                  
+                  children: [
+                  Icon(Icons.store),
+                  TextButton(
+                    onPressed: (){}, 
+                    child: Text(
+                      "Pick up",
+                      style: TextStyle(
+                        fontFamily: 'Noto Sans',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),
+                    )
+                    ),
+                    Spacer(),
+                    Icon(Icons.store),
+                  TextButton(
+                    onPressed: (){}, 
+                    child: Text(
+                      "Pick up",
+                      style: TextStyle(
+                        fontFamily: 'Noto Sans',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),
+                    )
+                    ),
+                  ]),
+              )
+            ],
+           ),
+        ],
+      ),
     );
   }
 }
